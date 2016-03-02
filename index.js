8cebdcd09448c0ed9171a7a646a6a3ac161e6c89
schuchard <kbs177@gmail.com>
2016-03-02 14:59:45 -0700

    clean up unused code

--- index.js
+++ index.js
@@ -17,36 +17,15 @@ io.on('connection', function (socket) {
   var canvasCommands = [];
 
   socket.on('whiteboard', function (request) {
-    // var connection = request.accept('whiteboard-example', request.origin);
-    // connections.push(connection);
-
-    // console.log(connection.remoteAddress + " connected - Protocol Version " + connection.webSocketVersion);
-    
-    // Send all the existing canvas commands to the new client
-    // connection.sendUTF(JSON.stringify({
-    //   msg: "initCommands",
-    //   data: canvasCommands
-    // }));
-    
     io.emit('draw', { msg: "initCommands", data: canvasCommands });
-
   });
   // Handle closed connections
   socket.on('close', function () {
-    console.log(socket.remoteAddress + " disconnected");
-    // var index = connections.indexOf(connection);
-    // if (index !== -1) {
-    //   // remove the connection from the pool
-    //   connections.splice(index, 1);
-    // }
+
   });
   
   // Handle incoming messages
   socket.on('message', function (message) {
-    // if (message.type === 'utf8') {
-    //   try {
-    //     var command = JSON.parse(message.utf8Data);
-
     if (message.msg === 'clear') {
       canvasCommands = [];
     }
@@ -55,11 +34,6 @@ io.on('connection', function (socket) {
     }
 
     io.emit('draw', message);
-    // // rebroadcast command to all clients
-    // connections.forEach(function (destination) {
-    //   // destination.sendUTF(message.utf8Data);
-    //   destination.emit(message.utf8Data);
-    // });
   });
 
 });
